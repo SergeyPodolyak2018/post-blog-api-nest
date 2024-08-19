9a00d5df491e3cea00c8be9e105ecef3f279bccf
Sergey Podolyak <sergey.dev.2024@gmail.com>
2024-08-19 11:30:54 +0300

    Fix swager

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -13,17 +13,32 @@ import {
 import { BlogService } from './blog.service';
 import { CreateBlogDto } from './dto/create.blog.dto';
 import { CONST_MESSAGES } from './blog.const';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ResponseBlogDto } from './dto/response.blog.dto';
 
+@ApiTags('Blog')
 @Controller('blog')
 export class BlogController {
   constructor(private readonly blogService: BlogService) {}
 
   @Post('create')
+  @ApiOperation({ summary: 'Create a post' })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Created',
+    type: ResponseBlogDto,
+  })
   async create(@Body() dto: CreateBlogDto) {
     return this.blogService.create(dto);
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get post by id' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Success',
+    type: ResponseBlogDto,
+  })
   async getBlogById(@Param('id') blogId: string) {
     const result = await this.blogService.findById(blogId);
     if (!result) {
@@ -34,11 +49,23 @@ export class BlogController {
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all posts' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Success',
+    type: [ResponseBlogDto],
+  })
   async getAllBlogs() {
     return this.blogService.findAll();
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete post by id' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Success',
+    type: ResponseBlogDto,
+  })
   async delete(@Param('id') id: string) {
     const deletedBlog = await this.blogService.deleteById(id);
     if (!deletedBlog) {
@@ -48,6 +75,12 @@ export class BlogController {
     }
   }
   @Put(':id')
+  @ApiOperation({ summary: 'Update post by id' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Success',
+    type: ResponseBlogDto,
+  })
   async putById(@Param('id') id: string, @Body() dto: CreateBlogDto) {
     const updatedBlog = await this.blogService.update(id, dto);
     if (!updatedBlog) {
--- src/blog/dto/response.blog.dto.ts
+++ src/blog/dto/response.blog.dto.ts
@@ -1,6 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class CreateBlogDto {
+export class ResponseBlogDto {
+  @ApiProperty({ description: 'Post id in db', nullable: false })
+  _id: string;
+  @ApiProperty({ description: 'Db internal field', nullable: false })
+  _v: string;
   @ApiProperty({ description: 'Post title', nullable: false })
   title: string;
   @ApiProperty({ description: 'Post description', nullable: false })
